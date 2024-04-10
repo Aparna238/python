2398949a282f9a67b4da257778c5ad6041f991b5
Aparna238 <84956955+Aparna238@users.noreply.github.com>
2024-04-10 15:29:51 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,52 @@
+import tkinter as tk
+import logging
+from tkinter import messagebox
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    filename='task.log'
+                    )
+
+class InvalidInput(Exception):
+    def __init__(self,message):
+        self.message = message
+
+def validate_input(input):
+    words = input.split()
+    if len(words) < 3:
+        raise InvalidInput("Please enter atleast 3 words")
+
+def process_input(user_input):
+    try:  
+        validate_input(user_input)
+
+        logging.info(f"Correct input recieved: {user_input}")
+        capitalize_next = True
+        processed_output = ''
+
+        for char in user_input:
+            if char != ' ': 
+                if capitalize_next:
+                    processed_output += char.upper()
+                else:
+                    processed_output += char.lower()
+                capitalize_next = not capitalize_next
+            else:
+                processed_output += char
+                
+        print("Modified string: ",processed_output)
+        logging.info(f"Output displayed: {processed_output}")
+
+    except InvalidInput as e:
+        logging.error("Wrong input received: Less than 3 words ")
+        print("Error!! Wrong input - less than 3 words")
+
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', '-i')
+    args = parser.parse_args()
+
+    if args.input and args.output:
+        logging.debug("Input via command line")
+        process_input(args.input)
